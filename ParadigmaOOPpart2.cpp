1ddfd2325c24672d48d8565cbc687179a2a13a81
ZizahSwiftie <azizahulnha@gmail.com>
2025-06-02 17:30:32 +0700

    feat: Tambah varian seseorang: joko dan lia

--- ParadigmaOOPpart2.cpp
+++ ParadigmaOOPpart2.cpp
@@ -11,3 +11,22 @@ public:
     }
 }; 
 
+class joko : public seseorang
+{   
+public:
+    void pesan()
+    {
+        cout << "Pesan dari joko" << endl;
+    }
+};
+
+class lia : public seseorang
+{   
+public:
+    void pesan()
+    {
+        cout << "Pesan dari lia" << endl;
+    }
+};
+
+
